e4eca0cdcc41f6b8187297daa067b5657b6cf268
Sungjae <sungjae.cho96@gmail.com>
2024-05-16 13:20:51 +0900

    first commit

---
+++ src/main.py
@@ -0,0 +1,21 @@
+import time
+
+from multiprocessing import Process
+
+def worker():
+    for i in range(100):
+        print(f">>> Woreker : {i}")
+        time.sleep(0.5)
+    
+if __name__ == "__main__":
+    p = Process(target=worker)
+    p.start()
+    
+    cnt = 0
+    while p.is_alive():
+        print(f">>> Main : {cnt} MainMainMainMainMainMainMainMainMainMainMain")
+        time.sleep(5)
+    p.join()
+# asyncio.run(coro)
+# if __name__ == "__main__":
+    
